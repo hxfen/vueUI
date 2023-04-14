2455085ec7361f68f38ec4e80f4afaeb8c3bd005
hxfen <870977925@qq.com>
2023-04-14 15:59:13 +0800

    feat:save

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,12 @@
 module.exports = {
     base: '/vueUI/',
-    // title: 'vueUI',
+    title: 'Vue-UI',
     themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'GitHub', link: 'https://github.com/hxfen/vueUI' },
+        { text: 'Npm', link: 'https://www.npmjs.com/search?q=vue-ui-template' }
+      ],
       sidebar: [
         '/',
         '/componentDocs/card',
